5824c7bf2bdacd6937edf625ed8b3cd10981d15e
Marshall Lentz <marshall@lentzfam.com>
2019-10-06 12:58:38 -0400

    Modify the program to print each name on its own line.

--- name_looper.py
+++ name_looper.py
@@ -1,3 +1,7 @@
-print ("Program")
+print ("Program that repeats your name a number of times")
 name = input("Enter your name: ")
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
+
+for i in range(0, num):
+	print(name)
+
